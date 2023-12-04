a98036a3d464faf81dbcf087c727492cd142fb11
Jihyeong Han <hjh000504@naver.com>
2023-12-04 11:17:23 +0900

    logic fix

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <StContainer>LoginPage</StContainer>;
 };
 
 export default LoginPage;
+
+const StContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100%;
+`;
